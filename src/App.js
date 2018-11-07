cb5a83ccc941289bdae9c7517c6b3aff154ee109
Oliver Gomes <ogomesphoto@gmail.com>
2018-11-07 15:22:54 -0500

    NavbarBrand implement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Navbar } from "reactstrap";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar color="primary" />
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }

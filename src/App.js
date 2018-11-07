0a37254d71c4e792e85ae05ac5e73ca64aab8ae0
Oliver Gomes <ogomesphoto@gmail.com>
2018-11-07 17:43:09 -0500

    state modify to gather dishes data from separate file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 import MenuComponent from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  state = {
+    dishes: DISHES
+  };
+
   render() {
     return (
       <div>
@@ -12,7 +17,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <MenuComponent />
+        <MenuComponent dishes={this.state.dishes} />
       </div>
     );
   }

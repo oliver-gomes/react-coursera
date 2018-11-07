ed0edb718bf58f18870c9cde36c688bfedc020cf
Oliver Gomes <ogomesphoto@gmail.com>
2018-11-07 18:16:41 -0500

    event handler to display seleted dish

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,31 +1,58 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle
+} from "reactstrap";
 
 class MenuComponent extends Component {
-  state = {};
+  state = {
+    selectedDish: null
+  };
+
+  onDishSelect = dish => {
+    this.setState({
+      selectedDish: dish
+    });
+  };
+
+  renderDish = dish => {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div />;
+    }
+  };
 
   render() {
     const menu = this.props.dishes.map(dish => {
       return (
-        <div className="col-12 mt-5" key={dish.id}>
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div className="col-12 col-md-5 m-1" key={dish.id}>
+          <Card onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
 
     return (
       <div className="container">
-        <div className="row">
-          <Media list>{menu}</Media>
-        </div>
+        <div className="row">{menu}</div>
+        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
       </div>
     );
   }
